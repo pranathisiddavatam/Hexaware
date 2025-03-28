5b2be4100aecfaf06213791704a6e6a58de2056f
pranathisiddavatam <113352841+pranathisiddavatam@users.noreply.github.com>
2025-03-28 14:10:51 +0530

    Update Assignment-2.sql

--- Assignments/Assignment-2.sql
+++ Assignments/Assignment-2.sql
@@ -3,11 +3,37 @@ USE SISDB;
 GO
 CREATE SCHEMA SIS;
 GO
---CREATE TABLE SIS.Students (student_id INT IDENTITY(1,1) PRIMARY KEY,first_name VARCHAR(50) NOT NULL,last_name VARCHAR(50) NOT NULL,date_of_birth DATE NOT NULL,email VARCHAR(100) UNIQUE NOT NULL,phone_number VARCHAR(15) UNIQUE NOT NULL);
---CREATE TABLE SIS.Courses (course_id INT IDENTITY(1,1) PRIMARY KEY,course_name VARCHAR(100) NOT NULL,credits INT NOT NULL CHECK (credits > 0),teacher_id INT,  FOREIGN KEY (teacher_id) REFERENCES Teacher(teacher_id) ON DELETE SET NULL);
---CREATE TABLE SIS.Enrollments (enrollment_id INT IDENTITY(1,1) PRIMARY KEY,student_id INT,course_id INT,enrollment_date DATE NOT NULL,FOREIGN KEY (student_id) REFERENCES Students(student_id) ON DELETE CASCADE,FOREIGN KEY (course_id) REFERENCES Courses(course_id) ON DELETE CASCADE);
---CREATE TABLE SIS.Teacher (teacher_id INT IDENTITY(1,1) PRIMARY KEY,first_name VARCHAR(50) NOT NULL,last_name VARCHAR(50) NOT NULL,email VARCHAR(100) UNIQUE NOT NULL);
---CREATE TABLE SIS.Payments (payment_id INT IDENTITY(1,1) PRIMARY KEY,student_id INT,amount DECIMAL(10,2) NOT NULL CHECK (amount > 0),payment_date DATE NOT NULL,FOREIGN KEY (student_id) REFERENCES Students(student_id) ON DELETE CASCADE);
+CREATE TABLE SIS.Students (
+      student_id INT IDENTITY(1,1) PRIMARY KEY,
+      first_name VARCHAR(50) NOT NULL,
+      last_name VARCHAR(50) NOT NULL,
+      date_of_birth DATE NOT NULL,
+      email VARCHAR(100) UNIQUE NOT NULL,
+      phone_number VARCHAR(15) UNIQUE NOT NULL);
+CREATE TABLE SIS.Courses (
+      course_id INT IDENTITY(1,1) PRIMARY KEY,
+      course_name VARCHAR(100) NOT NULL,
+      credits INT NOT NULL CHECK (credits > 0),
+      teacher_id INT,
+      FOREIGN KEY (teacher_id) REFERENCES Teacher(teacher_id) ON DELETE SET NULL);
+CREATE TABLE SIS.Enrollments (
+      enrollment_id INT IDENTITY(1,1) PRIMARY KEY,
+      student_id INT,
+      course_id INT,
+      enrollment_date DATE NOT NULL,
+      FOREIGN KEY (student_id) REFERENCES Students(student_id) ON DELETE CASCADE,
+      FOREIGN KEY (course_id) REFERENCES Courses(course_id) ON DELETE CASCADE);
+CREATE TABLE SIS.Teacher (
+      teacher_id INT IDENTITY(1,1) PRIMARY KEY,
+      first_name VARCHAR(50) NOT NULL,
+      last_name VARCHAR(50) NOT NULL,
+      email VARCHAR(100) UNIQUE NOT NULL);
+CREATE TABLE SIS.Payments (
+      payment_id INT IDENTITY(1,1) PRIMARY KEY,
+      student_id INT,
+      amount DECIMAL(10,2) NOT NULL CHECK (amount > 0),
+      payment_date DATE NOT NULL,
+      FOREIGN KEY (student_id) REFERENCES Students(student_id) ON DELETE CASCADE);
 INSERT INTO SIS.Students (first_name, last_name, date_of_birth, email, phone_number) VALUES
 ('John', 'Doe', '2002-05-15', 'john.doe@example.com', '9876543210'),
 ('Alice', 'Smith', '2001-09-22', 'alice.smith@example.com', '9876543211'),
